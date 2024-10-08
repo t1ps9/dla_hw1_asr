3aadfb0fe0fe229d797bbe431fc1d03a15529ea6
Timur Nig <tanigma03@mail.ru>
2024-10-08 21:53:00 +0300

    change wer2

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -16,10 +16,4 @@ def calc_wer(target_text, predicted_text) -> float:
     if len(target_text) == 0:
         return 1
 
-    if isinstance(target_text, str):
-        target_text = target_text.split(' ')
-
-    if isinstance(predicted_text, str):
-        predicted_text = predicted_text.split(' ')
-
-    return editdistance.eval(target_text, predicted_text) / len(target_text)
+    return editdistance.eval(target_text.split(), predicted_text.split()) / len(target_text.split())
