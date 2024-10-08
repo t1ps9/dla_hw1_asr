657c72b6530c3d98953e3c9f830c2de4fc91532d
Timur Nig <tanigma03@mail.ru>
2024-10-08 22:07:14 +0300

    change wer3

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -15,5 +15,7 @@ def calc_cer(target_text, predicted_text) -> float:
 def calc_wer(target_text, predicted_text) -> float:
     if len(target_text) == 0:
         return 1
-
+    print(target_text)
+    print('------')
+    print(predicted_text)
     return editdistance.eval(target_text.split(), predicted_text.split()) / len(target_text.split())
