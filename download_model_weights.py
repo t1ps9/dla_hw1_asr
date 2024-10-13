a79c3b7e148d7fa371ca4ddfe3ecea2f02149b0b
Timur Nig <tanigma03@mail.ru>
2024-10-13 21:24:44 +0300

    inference6

--- download_model_weights.py
+++ download_model_weights.py
@@ -7,6 +7,7 @@ def download():
     gdown.download("https://drive.google.com/uc?id=1OAvDuSfdgXOJ9FHsdo5CxTGZFiBL-FzN")
     gdown.download("https://drive.google.com/uc?id=1isFy_G-laxWCa9SYRTzDd_YtqFKfJXGr")
 
+    os.mkdir('to_download')
     os.rename("model_best.pth", "to_download/model_best.pth")
     os.rename("librispeech-vocab.txt", "to_download/librispeech-vocab.txt")
     os.rename("3-gram.pruned.1e-7.arpa", "to_download/3-gram.pruned.1e-7.arpa")
