e5ebba2bbf77d3f448dafa9945d5bbd3156ce817
Timur Nig <tanigma03@mail.ru>
2024-10-13 23:12:18 +0300

    change wer2

--- src/utils/io_utils.py
+++ src/utils/io_utils.py
@@ -30,3 +30,8 @@ def write_json(content, fname):
     fname = Path(fname)
     with fname.open("wt") as handle:
         json.dump(content, handle, indent=4, sort_keys=False)
+
+
+def read_txt(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return f.read()
